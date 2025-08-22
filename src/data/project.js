b257aca24a8ef2b9f8f5fafda5ce3d3f6f3c42ce
augustinegrepo <augustinegrepo@gmail.com>
2025-08-22 22:33:15 +0800

    Added Photos on Project Section

--- src/data/project.js
+++ src/data/project.js
@@ -1,32 +1,46 @@
 const projects = [
   {
-    photo: "https://scontent.fmnl3-4.fna.fbcdn.net/v/t39.30808-6/530368555_1334051145397076_1555502631121704701_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=f727a1&_nc_ohc=yHrozC2SVUUQ7kNvwG0aoqB&_nc_oc=AdntNcES5ujg9Ha48up4uT1FxkEjTYgOPxGJAcFfaacnoO5HTikJ6CmKIXFRZVOluSc&_nc_zt=23&_nc_ht=scontent.fmnl3-4.fna&_nc_gid=uBpb-jn6Islq1_62BPmMFQ&oh=00_AfU0Bv8C--FJ2tDC54HNMuHZX3j7YFOkpUChfY2yGTrzoA&oe=689F4800",
+    photo: "/images/projects/ecommerce/home.png",
     title: "E-Commerce Website Capstone Project",
     description: "E-Commerce Web App a capstone project requirement for Main Course Package of Zuitt Bootcamp Course.",
     tags: ["MongoDB", "Express JS", "React", "Node JS", "JWT Token", "CRUD Operations"],
     github: "https://github.com/augustdigiroom/E-Commerce-Full-Stack",
     website: "https://augustinegrepo-ecommercefullstack.vercel.app/product",
     screenshots: [
-      "https://scontent.fmnl37-2.fna.fbcdn.net/v/t39.30808-6/519255813_1334051325397058_1106238195879988357_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=f727a1&_nc_ohc=nZYJlfbXL-8Q7kNvwGAaeDr&_nc_oc=Adm-IrQQ_GBIyb1L2kjNo4EIFv3z42dIMx2dZiY2YABMYn1KQgDuk0HmoK-qCOGSTEY&_nc_zt=23&_nc_ht=scontent.fmnl37-2.fna&_nc_gid=ILAg1muYDO-Vwjf8H3aV-Q&oh=00_AfWTANkA5jAyo_wcWRpxw6dx5jqa14HEFkjt85TTXHweOQ&oe=689F5B90",
-      "https://scontent.fmnl3-4.fna.fbcdn.net/v/t39.30808-6/530257601_1334051328730391_1641391074553370287_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=f727a1&_nc_ohc=Yokg056bsX0Q7kNvwEFg52m&_nc_oc=AdnsiMbG9fEb4alWOkkzeqcaAG6jOwdMiyHdmCHjbe4zCdjcYaiG1mc7KwnivuJlrMU&_nc_zt=23&_nc_ht=scontent.fmnl3-4.fna&_nc_gid=qXWk6MNpTDHHfNGipPeejQ&oh=00_AfUsaNmlHS_zd6eT5_VQUZQbgubUcerb8SZfGgN5N6HuHg&oe=689F47F9",
-      "https://scontent.fmnl37-2.fna.fbcdn.net/v/t39.30808-6/530229422_1334051238730400_4824214741493406757_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=f727a1&_nc_ohc=yjkKTJLTn4oQ7kNvwENXO_k&_nc_oc=AdkQjBMXBbLBPIefNPh7taoIMEA28RfROiJ0TzUk233D2XDTQ8afjocz4GhkAwZJ7-I&_nc_zt=23&_nc_ht=scontent.fmnl37-2.fna&_nc_gid=OHl7qlZxxsDofpcgodtE2g&oh=00_AfW-VG5elnPcAtPhYniG9i_tsfMn-vMLLjnh5adgMB9D4A&oe=689F3B67",
-      "https://scontent.fmnl3-4.fna.fbcdn.net/v/t39.30808-6/530368555_1334051145397076_1555502631121704701_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=f727a1&_nc_ohc=yHrozC2SVUUQ7kNvwG0aoqB&_nc_oc=AdntNcES5ujg9Ha48up4uT1FxkEjTYgOPxGJAcFfaacnoO5HTikJ6CmKIXFRZVOluSc&_nc_zt=23&_nc_ht=scontent.fmnl3-4.fna&_nc_gid=uBpb-jn6Islq1_62BPmMFQ&oh=00_AfU0Bv8C--FJ2tDC54HNMuHZX3j7YFOkpUChfY2yGTrzoA&oe=689F4800",
-      "https://scontent.fmnl37-2.fna.fbcdn.net/v/t39.30808-6/530264761_1334051138730410_4255658125206056745_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=f727a1&_nc_ohc=RPo0s1hQCr4Q7kNvwEuEftG&_nc_oc=AdnFX_nn6MCVXGRM8bucYezKsLEMbaRx6g2IWVwve5KNejz1SXsPg_8BkGWNjTI7Di8&_nc_zt=23&_nc_ht=scontent.fmnl37-2.fna&_nc_gid=uBpb-jn6Islq1_62BPmMFQ&oh=00_AfXi6ni5pKHdKJz3hGzLqWn8PJHcs_U22GFO293Rt4EWfg&oe=689F6CD4"
+      "/images/projects/ecommerce/home.png",
+      "/images/projects/ecommerce/register.png",
+      "/images/projects/ecommerce/login.png",
+      "/images/projects/ecommerce/loginsuccessful.png",
+      "/images/projects/ecommerce/admindashboard.png",
+      "/images/projects/ecommerce/addnewproduct.png",
+      "/images/projects/ecommerce/admincannotaddtocart.png",
+      "/images/projects/ecommerce/customerproductpage.png",
+      "/images/projects/ecommerce/addtocart.png",
+      "/images/projects/ecommerce/confirmpayment.png"
     ],
     longDescription: ""
   },
   {
-    photo: "https://images.unsplash.com/photo-1465101178521-c1a4c8a0a8b7?auto=format&fit=crop&w=600&q=80",
+    photo: "/images/projects/coursebooking/home.png",
     title: "Course Booking System - Zuitt Bootcamp Course",
     description: "A web application for booking courses, managing schedules, and user accounts.",
     tags: ["MongoDB", "Express JS", "React", "Node JS", "JWT Token", "CRUD Operations"],
-    github: "https://github.com/yourusername/task-manager",
-    website: "https://taskmanagerapp.com",
+    github: "https://github.com/augustdigiroom/Course-Booking-with-React-JS",
+    website: "https://github.com/augustdigiroom/Course-Booking-with-React-JS",
     screenshots: [
-      "https://images.unsplash.com/photo-1465101178521-c1a4c8a0a8b7?auto=format&fit=crop&w=600&q=80",
-      "https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=600&q=80"
+      "/images/projects/coursebooking/home.png",
+      "/images/projects/coursebooking/featured.png",
+      "/images/projects/coursebooking/homenews.png",
+      "/images/projects/coursebooking/news.png",
+      "/images/projects/coursebooking/courses.png",
+      "/images/projects/coursebooking/register.png",
+      "/images/projects/coursebooking/login.png",
+      "/images/projects/coursebooking/admindashboard.png",
+      "/images/projects/coursebooking/addcourse.png",
+      "/images/projects/coursebooking/addnews.png",
+      "/images/projects/coursebooking/editcourse.png"
     ],
-    longDescription: "this is edit, edit again, edit againnn"
+    longDescription: ""
   },
   {
     photo: "https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=600&q=80",
